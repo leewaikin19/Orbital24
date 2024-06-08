820f9204954d6b7df062dd51338b6e50dc2eb1d8
Lee Wai Kin <leewaikin19@gmail.com>
2024-06-08 20:45:58 +0800

    a

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ export default function App() {
       <Routes>
           <Route path="/">
             <Route path="" element={<Main />} />
+            <Route path="another" element={<Main />} />
             <Route path="home" element={<Home />} />
             <Route path="*" element={<P404 />} />
           </Route>
