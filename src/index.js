32e30f357e8311ab663868f685db6d1c1696cd28
Lee Wai Kin <leewaikin19@gmail.com>
2024-06-08 20:47:48 +0800

    a

--- src/index.js
+++ src/index.js
@@ -25,8 +25,8 @@ export default function App() {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  //<React.StrictMode>
     
     <App />
-  </React.StrictMode>
+  //</React.StrictMode>
 );
