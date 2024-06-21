569a96c2190bf7a00f8bc1313fd8e781223dd124
Dominic Bryan <dominicbryangunawan@gmail.com>
2024-06-21 19:40:54 +0800

    Finished Simulation of POP

--- src/pigeonhole.js
+++ src/pigeonhole.js
@@ -12,21 +12,67 @@ export default function PigeonHole() {
         console.log('Im doneee')
     })
     return (
-        < template.Home MainContent={() => (<MainContent title={"No Bad Luck Lasts Forever"} description={"There are 52 cards (a standard deck) on the table. Unluckily, you are trapped in a dark room and cannot see which side the cards are facing. You can take as many cards as you want, and you win if there is at least one card from each suit. How many cards should you take to guarantee a win?"} />)} promise={promise} />
+        < template.Home MainContent={() => (<MainContent title={"Bad Luck Doesn't Last Forever"} description={"There are 52 cards (a standard deck) on the table. Unluckily, you are trapped in a dark room and cannot see which side the cards are facing. You can take as many cards as you want, and you win if there is at least one card from each suit. How many cards should you take to always guarantee a win?"} />)} promise={promise} />
     ) 
 }
 
 function MainContent({title, description, id}) { 
-    var outer_card_array = []
     function cardSimulation(cards) {
-        const card_container = document.getElementById('card_container');
-        card_container.innerHTML = '';
-        for (let i = 0; i < cards.length; i++) {
-            var card = document.createElement('img');
-            card.setAttribute("src", "../../Assets/Cards/" + cards[i] + ".svg");
-            card_container.appendChild(card);
-        }
+        cards >=1 && cards <= 52 ? cards : cards = 1;
+        const card_array = [];
+        const suits = ['clubs', 'hearts', 'diamonds', 'spades'];
+        const clubs = document.getElementById('clubs');
+        clubs.innerHTML = '';
+        const hearts = document.getElementById('hearts');
+        hearts.innerHTML = '';
+        const spades = document.getElementById('spades');
+        spades.innerHTML = '';
+        const diamonds = document.getElementById('diamonds');
+        diamonds.innerHTML = '';
+        var clubs_count = 0;
+        var hearts_count = 0;
+        var spades_count = 0;
+        var diamonds_count = 0;
 
+        for(let i = 0; i < cards; i++) {
+            var n = Math.floor(Math.random() * 52);
+            var suit = suits[Math.floor(n/13)];
+            var value = n % 13 + 1;
+            var s = String(value) + "_" + suit;
+            if(card_array.includes(s)) { 
+                i--;
+                continue;
+            } else{
+                card_array.push(s);
+                var card = document.createElement('img');
+                card.setAttribute("src", "../../Assets/Cards/" + s + ".svg");
+                card.setAttribute("width", "100vw");
+                switch (suit) {
+                    case 'clubs':
+                        clubs.appendChild(card);
+                        clubs_count++;
+                        console.log(clubs_count);
+                        break;
+                    case 'hearts':
+                        hearts.appendChild(card);
+                        hearts_count++;
+                        break;
+                    case 'spades':
+                        spades.appendChild(card);
+                        spades_count++;
+                        break;
+                    case 'diamonds':
+                        diamonds.appendChild(card);
+                        diamonds_count++;
+                        break;
+                }
+            }
+            if(clubs_count * hearts_count * spades_count * diamonds_count == 0) {
+                document.getElementById('sucess_message').innerHTML = 'Sadly there is one or more missing suit. Try again!';
+            } else {
+                document.getElementById('sucess_message').innerHTML = 'You succeeded. Congratulations! But do you really need to take this many cards? Or is this not enough to guarantee a win?';
+            }
+        }
     }
     return (
         <div className='problems'>
@@ -42,20 +88,17 @@ function MainContent({title, description, id}) {
                     <div className='form_input' style={{display:"flex", flexDirection:"row", alignItems:"center", justifyContent:"center"}}>
                         <input style={{flexGrow:"1", textAlign:"center"}} type="number" name="cards" min="1" max="52" placeholder='Enter how many cards you will take' required/>
                         <button style={{flexGrow:"1", marginTop:"0px"}} className="action_button animated_button" onClick={() => {
-                            const cards = document.querySelector('input[name="cards"]').value;
-                            var card_array = [];
-                            if(cards >= 1 && cards <= 52) {
-                                const suits = ['clubs', 'hearts', 'diamonds', 'spades'];
-                                for(let i = 0; i < cards; i++) {
-                                    const s = String(Math.floor(Math.random() * 12 + 1)) + "_" + String(suits[Math.floor(Math.random() * 4)])
-                                    card_array.includes(s) ? i-- : card_array.push(s);
-                                }
-                                console.log(card_array)
-                                cardSimulation(card_array);
-                            }
+                            const cards = document.querySelector('input[name="cards"]').value
+                            cardSimulation(cards)
                         }}><span>Take Cards</span></button>
                     </div>
-                    <div id='card_container' style={{display:"flex", flexWrap:"wrap", justifyContent:"space-around", margin:"1vh 1vw"}}>
+                    <div id='card_container' style={{display:"flex", flexDirection:"column", margin:"1vh 1vw", rowGap:"0.5vh"}}>
+                        <div id='clubs' style={{display:"flex", flexDirection:"row", columnGap:"0.5vw"}}></div>
+                        <div id='hearts' style={{display:"flex", flexDirection:"row", columnGap:"0.5vw"}}></div>
+                        <div id='spades' style={{display:"flex", flexDirection:"row", columnGap:"0.5vw"}}></div>
+                        <div id='diamonds' style={{display:"flex", flexDirection:"row", columnGap:"0.5vw"}}></div>
+                    </div>
+                    <div id='sucess_message' style={{textAlign:"center"}}>
                     </div>
                 </>
             }/>
